dcc2c797e47b5c8ee33b69700171d573abf1d41b
Bhuwan  Singh Bist <151565229+Genomics-Bhuwan@users.noreply.github.com>
2026-03-22 10:46:18 -0400

    Add initial script for finding deleterious mutations

---
+++ VEP_Emilyscript.sh
@@ -0,0 +1,6 @@
+#### Finding deleterious mutation in the  Dama gazelle.
+- Usually kept three species but here I am keeping two outgroups.
+- Grant’s gazellehttps://trace.ncbi.nlm.nih.gov/Traces/?run=SRR6878810)  and Thomson's gazelle (https://trace.ncbi.nlm.nih.gov/Traces/?run=SRR6894844)
+#### Step 1. Download the short-read sequences of Grant's gazelle and Thomson's gazelle.
+- Map these with BWA-MEM using the reference genome assembly of Dama gazelle.
+- 
